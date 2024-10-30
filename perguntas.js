9b78c66b48b7f06086ebbce805ca67742e2a5fbb
eduardooligini <145785071+eduardooligini@users.noreply.github.com>
2024-10-30 09:40:27 -0300

    000000

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Vôlei'
+    'Quantos jogadores fazem parte de uma equipe?'
+    '12 jogadores, 6 titulares e 6 reservas.'
+)
+
+criaCartao(
+    'Vôlei'
+    'Quem criou o vôlei?'
+    'William G. Morgan'
+)
+
+criaCartao(
+    'Vôlei'
+    'Quais são os fundamentos do vôlei?'
+    'Saque, ataque, bloqueio, levantamento e recepção.'
+)
+
+criaCartao(
+    'Vôlei'
+    'Qual o set mais longo da história do vôlei?'
+    'No Pré-Olímpico do Japão, a França também esteve envolvida no set mais longo da história do vôlei. Diante da Austrália, o placar ficou em 44 a 42 para os franceses em 1h43.'
+)
